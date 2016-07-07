89a8a4d73db2828a96981e7dd3eb2bca1c6a261b
Jisse Reitsma <info@yireo.com>
2016-07-07 13:32:30 +0200

    Initial commit

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    paths:{
+        "jquery.bootstrap":"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min"
+    },
+    shim:{
+        'jquery.bootstrap':{
+            'deps':['jquery']
+        }
+    }
+};
